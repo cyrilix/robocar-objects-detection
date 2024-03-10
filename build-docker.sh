8a89d9e4be831bbf5f6e6611c299cd886c1a1fde
Cyrille Nofficial <cynoffic@cyrilix.fr>
2024-03-10 11:25:57 +0100

    cicd: fix image build

--- build-docker.sh
+++ build-docker.sh
@@ -5,5 +5,8 @@ IMAGE_NAME=robocar-objects-detection
 TAG=$(git describe)
 FULL_IMAGE_NAME=docker.io/cyrilix/${IMAGE_NAME}:${TAG}
 
+# Remove old images
+podman manifest rm localhost/${IMAGE_NAME}:${TAG} ${FULL_IMAGE_NAME}
+
 podman build . --platform linux/amd64,linux/arm64 --manifest ${IMAGE_NAME}:${TAG}
-podman manifest push --all localhost/${IMAGE_NAME}:${TAG} ${FULL_IMAGE_NAME}
+podman manifest push --all --rm localhost/${IMAGE_NAME}:${TAG} ${FULL_IMAGE_NAME}
